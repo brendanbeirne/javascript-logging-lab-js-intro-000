bd5ca4bdd05d97feeea902318a061f4480194264
Brendan J Beirne <brendan.beirne@gmail.com>
2020-11-06 07:05:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.error("FML!")
 console.log("Hey there.")
 console.warn("Watch out!")
+
+var greeting = "";
+var specialGuest = "Neil Young"
+var greetSpecialGuest = "" + specialGuest + "!";
+var topic = "space";
+var conversation = ${topic};
